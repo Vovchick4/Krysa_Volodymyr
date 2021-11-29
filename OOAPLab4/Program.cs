9e0ca6b14be6f2a49407640badf4a4efc3cccde9
Vovchick4 <vovaspodpivas@gmail.com>
2021-11-29 11:00:30 +0200

    changes

---
+++ OOAPLab4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOAPLab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Adapter adapter = new Adapter();
+            adapter.MakeCall();
+            Console.Read();
+        }
+        interface Phone
+        {
+            void MakeCall();
+        }
+        class VideoCamera
+        {
+            public void UseVideo()
+            {
+                Console.WriteLine("Камера використовується при виклику");
+            }
+        }
+        class Adapter : Phone
+        {
+
+            VideoCamera videoclass = new VideoCamera();
+            public void MakeCall()
+            {
+                videoclass.UseVideo();
+            }
+        }
+    }
+}
